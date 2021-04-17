44b0cc55636f03efa9bde59f97bf4df840c87b7c
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-17 20:22:21 +0700

    Menambahkan fitur untuk mengambil gambar

--- app/src/main/java/umn/ac/id/FormBaru.java
+++ app/src/main/java/umn/ac/id/FormBaru.java
@@ -2,19 +2,28 @@ package umn.ac.id;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FormBaru extends AppCompatActivity {
+    private final int cameraRequestCode = 1;
+    private Button btnKembaliMenu, btnKamera;
+    private ImageView placeholderGambar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_baru);
-        Button btnKembaliMenu;
+
         btnKembaliMenu = findViewById(R.id.KembaliMenu);
+        btnKamera = findViewById(R.id.btnFoto);
+        placeholderGambar = findViewById(R.id.placeholderGambar);
 
         btnKembaliMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +31,30 @@ public class FormBaru extends AppCompatActivity {
                 openKembaliMenu();
             }
         });
+
+        btnKamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentAmbilFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                try {
+                    startActivityForResult(intentAmbilFoto, cameraRequestCode);
+                } catch (ActivityNotFoundException e) {
+                    // abaikan untuk sekarang...
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == cameraRequestCode && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap gambar = (Bitmap) extras.get("data");
+            placeholderGambar.setImageBitmap(gambar);
+        }
     }
 
     public void openKembaliMenu (){
