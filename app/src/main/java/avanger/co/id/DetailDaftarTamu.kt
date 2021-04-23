b2c4d812c00ba6044151e39f02e8ff21ef4e8266
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-23 10:37:49 +0700

    Implement status change

--- app/src/main/java/avanger/co/id/DetailDaftarTamu.kt
+++ app/src/main/java/avanger/co/id/DetailDaftarTamu.kt
@@ -2,9 +2,17 @@ package avanger.co.id
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_detail_daftar_tamu.*
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.concurrent.schedule
 
 class DetailDaftarTamu : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_daftar_tamu)
@@ -19,9 +27,33 @@ class DetailDaftarTamu : AppCompatActivity() {
             detailTamuPlat.text = tamu.platTamu
             detailTamuJamMasuk.text = tamu.jamMasuk.toString()
             detailTamuJamKeluar.text = tamu.jamKeluar.toString()
+
+            // Setup Firebase.
+            database = FirebaseDatabase.getInstance().reference
         }
 
         // Delegate listeners.
         btnKembali.setOnClickListener { finish() }
+        btnSubmit.setOnClickListener {
+            val currentItem = tamu?.idTamu?.let { id ->
+                database.child(getString(R.string.firebase_document))
+                        .child(getString(R.string.firebase_document))
+                        .child(id)
+            }
+
+            val updatedData = HashMap<String, Any>()
+            updatedData["jamKeluar"] = System.currentTimeMillis() / 1000L
+            updatedData["didalamKompleks"] = false;
+
+            currentItem?.updateChildren(updatedData)?.addOnCompleteListener {
+                Snackbar.make(detailDaftarTamu, getString(R.string.detail_tamu_success), Snackbar.LENGTH_LONG).show()
+
+                Timer().schedule(1000) {
+                    finish()
+                }
+            }?.addOnFailureListener {
+                Snackbar.make(detailDaftarTamu, getString(R.string.detail_tamu_failed), Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
