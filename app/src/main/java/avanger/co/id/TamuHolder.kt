c0bdca04e2ff48a74c9e541a30a1beddf56722bd
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-27 10:03:12 +0700

    Implement 'bind' method for reusability

--- app/src/main/java/avanger/co/id/TamuHolder.kt
+++ app/src/main/java/avanger/co/id/TamuHolder.kt
@@ -1,5 +1,7 @@
 package avanger.co.id
 
+import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -8,4 +10,15 @@ class TamuHolder(view: View) : RecyclerView.ViewHolder(view) {
     val tvNama: TextView = view.findViewById(R.id.namaTamu)
     val tvPlat: TextView = view.findViewById(R.id.platNomor)
     val dataHolder: View = view.findViewById(R.id.dataHolder)
+
+    fun bind(tamu: Tamu, context: Context) {
+        tvNama.text = tamu.namaTamu
+        tvPlat.text = tamu.platTamu
+
+        itemView.setOnClickListener {
+            val intent = Intent(itemView.context, DetailDaftarTamu::class.java)
+            intent.putExtra("tamu", tamu)
+            itemView.context.startActivity(intent)
+        }
+    }
 }
