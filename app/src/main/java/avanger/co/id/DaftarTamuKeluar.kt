24520f964dcf7b7540e3705bd4ceefed09fb525e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-27 10:47:07 +0700

    Modify redirection of 'DaftarTamuKeluar'

--- app/src/main/java/avanger/co/id/DaftarTamuKeluar.kt
+++ app/src/main/java/avanger/co/id/DaftarTamuKeluar.kt
@@ -1,33 +1,34 @@
 package avanger.co.id
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.firebase.ui.database.paging.DatabasePagingOptions
-import com.firebase.ui.database.paging.FirebaseRecyclerPagingAdapter
-import com.firebase.ui.database.paging.LoadingState
+import com.firebase.ui.firestore.paging.FirestorePagingAdapter
+import com.firebase.ui.firestore.paging.FirestorePagingOptions
+import com.firebase.ui.firestore.paging.LoadingState
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_daftar_tamu_keluar.*
+import java.lang.Exception
 
 class DaftarTamuKeluar : AppCompatActivity() {
-     private lateinit var adapter: FirebaseRecyclerPagingAdapter<Tamu, TamuHolder>
+     private lateinit var adapter: FirestorePagingAdapter<Tamu, TamuHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar_tamu_keluar)
 
+        // Initialize adapter.
+        adapter = tamuAdapter()
+
         // RecyclerView.
         tamuRecyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = tamuAdapter(this)
         tamuRecyclerView.adapter = adapter
 
         // Snackbar.
@@ -47,22 +48,22 @@ class DaftarTamuKeluar : AppCompatActivity() {
         adapter.stopListening()
     }
 
-    private fun tamuAdapter(ctx: Context): FirebaseRecyclerPagingAdapter<Tamu, TamuHolder> {
+    private fun tamuAdapter(): FirestorePagingAdapter<Tamu, TamuHolder> {
         // FirebaseUI preparations.
-        val database = FirebaseUtils.getFirebaseInstance()
-        val query = database.reference.child(getString(R.string.firebase_document))
+        val db = Firebase.firestore.collection(getString(R.string.firebase_document))
+        val query = db.orderBy("jamKeluar", Query.Direction.DESCENDING).whereEqualTo("didalamKompleks", false)
         val config = PagedList.Config.Builder()
                 .setEnablePlaceholders(false)
-                .setPrefetchDistance(3)
+                .setPrefetchDistance(5)
                 .setPageSize(10)
                 .build()
-        val options = DatabasePagingOptions.Builder<Tamu>()
+        val options = FirestorePagingOptions.Builder<Tamu>()
                 .setLifecycleOwner(this)
                 .setQuery(query, config, Tamu::class.java)
                 .build()
 
         // Returns a closure containing 'FirebaseRecyclerAdapter'.
-        return object : FirebaseRecyclerPagingAdapter<Tamu, TamuHolder>(options) {
+        return object : FirestorePagingAdapter<Tamu, TamuHolder>(options) {
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TamuHolder {
                 val view = LayoutInflater.from(parent.context)
                         .inflate(R.layout.item, parent, false)
@@ -71,22 +72,17 @@ class DaftarTamuKeluar : AppCompatActivity() {
             }
 
             override fun onBindViewHolder(holder: TamuHolder, position: Int, model: Tamu) {
-                holder.tvNama.text = model.namaTamu
-                holder.tvPlat.text = model.platTamu
-
-                holder.dataHolder.setOnClickListener {
-                    val intent = Intent(ctx, DetailTamuKeluar::class.java)
-                    intent.putExtra("tamu", model)
-                    ctx.startActivity(intent)
-                }
+                holder.bindTamuKeluar(model)
             }
 
-            override fun onError(error: DatabaseError) {
-                super.onError(error)
+            override fun onError(e: Exception) {
+                super.onError(e)
                 Snackbar.make(daftarTamu, getString(R.string.daftar_tamu_failed), Snackbar.LENGTH_LONG).show()
             }
 
             override fun onLoadingStateChanged(state: LoadingState) {
+                super.onLoadingStateChanged(state)
+
                 when (state) {
                     LoadingState.LOADING_INITIAL -> progressBar.visibility = View.VISIBLE
                     LoadingState.LOADING_MORE -> progressBar.visibility = View.VISIBLE
--- app/src/main/java/avanger/co/id/TamuHolder.kt
+++ app/src/main/java/avanger/co/id/TamuHolder.kt
@@ -6,9 +6,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TamuHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val tvNama: TextView = view.findViewById(R.id.namaTamu)
-    val tvPlat: TextView = view.findViewById(R.id.platNomor)
-    val dataHolder: View = view.findViewById(R.id.dataHolder)
+    private val tvNama: TextView = view.findViewById(R.id.namaTamu)
+    private val tvPlat: TextView = view.findViewById(R.id.platNomor)
 
     fun bindTamuMasuk(tamu: Tamu) {
         tvNama.text = tamu.namaTamu
@@ -16,6 +15,17 @@ class TamuHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         itemView.setOnClickListener {
             val intent = Intent(itemView.context, DetailTamuMasuk::class.java)
+            intent.putExtra("tamu", tamu)
+            itemView.context.startActivity(intent)
+        }
+    }
+
+    fun bindTamuKeluar(tamu: Tamu) {
+        tvNama.text = tamu.namaTamu
+        tvPlat.text = tamu.platTamu
+
+        itemView.setOnClickListener {
+            val intent = Intent(itemView.context, DetailTamuKeluar::class.java)
             intent.putExtra("tamu", tamu)
             itemView.context.startActivity(intent)
         }
